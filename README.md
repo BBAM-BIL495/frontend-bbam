a783014b1ff115d56fdfb358de764140c13c1c44
Betül Sağlam <149970112+betulsaglam@users.noreply.github.com>
2026-02-21 14:31:23 +0300

    feat: added common text input, fixed some dependency issues (#4)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Frontend repository of the Body &amp; Beyond AI Mentor Application - an AI-Power
 
 ### To work on the project,
 1. install [node.js](https://nodejs.org/). run `node -v`, `npm -v` and `npx -v` to verify installations
-2. under the project folder, run `npm i`
+2. under the project folder, run `npm install expo` then `npx expo install`. you can ignore vulnerabilities
 3. under the project folder, run `npx expo start` to start the project, you will see a qr code on the terminal
 4. install expo go from play store/app store on your phone and scan the qr code.
 5. now you can change some code, save it and see the changes real-time on your phone
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,28 @@ module.exports = {
           },
           inactive: "#9DA3A9"
         }
+      },
+      fontSize: {
+        'm3-headline-large': ['32px', { lineHeight: '40px', fontWeight: '400' }],
+        'm3-headline-large-emphasized': ['32px', { lineHeight: '40px', fontWeight: '600' }],
+        'm3-headline-medium': ['28px', { lineHeight: '36px', fontWeight: '400' }],
+        'm3-headline-medium-emphasized': ['28px', { lineHeight: '36px', fontWeight: '600' }],
+        'm3-headline-small': ['24px', { lineHeight: '32px', fontWeight: '400' }],
+        'm3-headline-small-emphasized': ['24px', { lineHeight: '32px', fontWeight: '600' }],
+
+        'm3-label-large': ['14px', { lineHeight: '20px', letterSpacing: '0.1px', fontWeight: '400' }],
+        'm3-label-large-emphasized': ['14px', { lineHeight: '20px', letterSpacing: '0.1px', fontWeight: '600' }],
+        'm3-label-medium': ['12px', { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '400' }],
+        'm3-label-medium-emphasized': ['12px', { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '600' }],
+        'm3-label-small': ['11px', { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '400' }],
+        'm3-label-small-emphasized': ['11px', { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '600' }],
+
+        'm3-body-large': ['16px', { lineHeight: '24px', letterSpacing: '0.5px', fontWeight: '400' }],
+        'm3-body-large-emphasized': ['16px', { lineHeight: '24px', letterSpacing: '0.5px', fontWeight: '600' }],
+        'm3-body-medium': ['14px', { lineHeight: '20px', letterSpacing: '0.25px', fontWeight: '400' }],
+        'm3-body-medium-emphasized': ['14px', { lineHeight: '20px', letterSpacing: '0.25px', fontWeight: '600' }],
+        'm3-body-small': ['12px', { lineHeight: '16px', letterSpacing: '0.4px', fontWeight: '400' }],
+        'm3-body-small-emphasized': ['12px', { lineHeight: '16px', letterSpacing: '0.4px', fontWeight: '600' }],
       }
     },
   },
